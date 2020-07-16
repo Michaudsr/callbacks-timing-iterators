e2ab249b7a7764b3d70818f203fa7d0a5e71749e
Steven Michaud <stevenmichaud@Stevens-MacBook-Air.local>
2020-07-16 18:32:08 -0400

    [callback] Add calculator function

---
+++ app.js
@@ -0,0 +1,67 @@
+// function greeting(name, callback) {
+//     let sentence = 'Hello, my name is' + name;
+
+//     return sentence;
+
+//     let otherSentence= callback();
+
+//     return sentence + otherSentence;
+// }
+
+// function anotherGreeting(){
+//     let sentence = 'I forgot my last name';
+    
+//     return sentence;
+// }
+
+// let myName = 'Steven';
+// console.log(greeting(myName, anotherGreeting));
+
+
+// function printArray(array, callback){
+//     for (let i =0; i < array.length; i++){
+//         let number = array[i];
+//     }
+
+//         console.log(number);
+// }    
+
+//         callback(array);
+// }
+
+
+// function printArrayAgain(array) {
+//     for (let i = 0; i < array.length; i++){
+//         let number = array[i];
+
+//         console.log(number);
+
+//     }
+// }
+
+// const numbersArray = [1,2,3,4,5,9,8,7,6]
+
+// printArray(numberArray, printArrayAgain);
+
+
+
+function calculator(num1, num2, callbackOne, callbackTwo)
+
+    Let result = callbackOne(num1,num2) + callbackTwo(num1,num2);
+
+    return result;
+}
+
+function addNumbers(num1, num2) {
+
+    let result = num1 + num2;
+
+    return result;
+
+}
+function subractNumber(num1, num2){
+    
+
+}
+
+console.log(calculator(5,77, addNumbers, subtractNumbers));
